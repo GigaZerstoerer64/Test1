2cb4d96f9fdfd42ca772bace2627084590f3aab8
GigaZerstoerer64 <gritschnederp14@isgy-schueler.de>
2026-05-08 10:01:29 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,3 +1,11 @@
 Task = input("Enter a simple math question")
 if Task[2] == "+"
   print("The solution is", float(Task[1]) + float(Task[3]))
+if Task[2] == "-"
+  print("The solution is", float(Task[1]) - float(Task[3]))
+if Task[2] == "*"
+  print("The solution is", float(Task[1]) * float(Task[3]))
+if Task[2] == "/"
+  print("The solution is", float(Task[1]) / float(Task[3]))
+if Task[2] == "%"
+  print("The solution is", float(Task[1]) & float(Task[3]))
